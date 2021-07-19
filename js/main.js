774ec5bf689c0349d0459b46f59dfa4dad904846
khc418 <73722334+khc418@users.noreply.github.com>
2021-07-19 14:37:13 +0900

    finish

--- js/main.js
+++ js/main.js
@@ -133,3 +133,11 @@ new Swiper('.awards .swiper-container', {
 
 const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear();
+
+const toTopEl = document.querySelector('#to-top')
+toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+});
